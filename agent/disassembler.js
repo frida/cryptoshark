a010292ef2bb463a1740f675b464ad58c244fedd
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-09-07 16:01:51 +0200

    Gracefully handle attempts to diassemble at an invalid address
    This should be incorporated into Instruction.parse() in a future Frida
    release.

--- agent/disassembler.js
+++ agent/disassembler.js
@@ -20,6 +20,14 @@ Disassembler.prototype.disassemble = function (func) {
         const result = [];
 
         let address = ptr(func.address);
+
+        try {
+            Memory.readU8(address);
+        } catch (e) {
+            resolve([]);
+            return;
+        }
+
         while (result.length < 100) {
             const insn = Instruction.parse(address);
             if (insn === null) {
