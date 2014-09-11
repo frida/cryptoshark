a7de276cd94a2ac81cfc3bffd383f0504c03d348
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-09-11 23:22:18 +0200

    Add QT_PREFIX to Windows environment

--- tools/env.bat
+++ tools/env.bat
@@ -2,12 +2,13 @@
 
 @cd %~dp0..\..\
 set CRYPTOSHARK_PREFIX=%CD%\dist
+set QT_PREFIX=%CD%\qt5\qtbase
 
 set VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0\
 set VCINSTALLDIR=%VSINSTALLDIR%VC\
 set SDKINSTALLDIR=C:\Program Files (x86)\Windows Kits\8.1\
 
-set PATH=%CD%\qt5\qtbase\bin;C:\Program Files\Python27;C:\Program Files\Perl\bin;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\nasm;%SDKINSTALLDIR%bin\x86;%VCINSTALLDIR%bin;%VSINSTALLDIR%Common7\IDE;%PATH%
+set PATH=%QT_PREFIX%\bin;C:\Program Files\Python27;C:\Program Files\Perl\bin;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\nasm;%SDKINSTALLDIR%bin\x86;%VCINSTALLDIR%bin;%VSINSTALLDIR%Common7\IDE;%PATH%
 set INCLUDE=%SDKINSTALLDIR%Include\um;%SDKINSTALLDIR%Include\shared;%VCINSTALLDIR%include
 set LIB=%SDKINSTALLDIR%Lib\winv6.3\um\x86;%VCINSTALLDIR%lib
 
