b36f53353917474e5ff1a3058bc2921fa42730bb
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-09-25 02:33:22 +0200

    Update the Qt build script for Mac

--- tools/01-build-qt-mac.sh
+++ tools/01-build-qt-mac.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-perl -pi.bak -e "s,-O2,-Os,g" mkspecs/common/gcc-base.conf
-perl -pi.bak -e "s,-O3,-Os,g" mkspecs/common/gcc-base.conf
-unset QMAKESPEC
-./configure -prefix $HOME/Qt/5.3/clang_64_static -opensource -confirm-license -release -static -no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci -no-sql-odbc -no-sql-psql -no-sql-tds -nomake examples -nomake tests -qt-zlib -qt-libpng -qt-libjpeg -no-freetype -no-harfbuzz -openssl-linked -no-icu -no-fontconfig -no-dbus
+./configure -prefix $HOME/Qt/5.3/clang_64_static -opensource -confirm-license -release -force-debug-info -no-strip -static -no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci -no-sql-odbc -no-sql-psql -no-sql-tds -nomake examples -nomake tests -qt-zlib -qt-libpng -qt-libjpeg -no-freetype -no-harfbuzz -openssl-linked -no-icu -no-fontconfig -no-dbus
+make -j8
