cadba302c5de40c00f04ed7022b2f1e57bf08f21
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-07-27 04:13:04 +0200

    Improve get-qt.bat to only fetch what we need

--- tools/windows/get-qt.bat
+++ tools/windows/get-qt.bat
@@ -1,5 +1,5 @@
 git clone git://code.qt.io/qt/qt5.git || exit /b
 cd qt5
 git checkout 5.15 || exit /b
-perl init-repository --module-subset=essential,qtquickcontrols || exit /b
+perl init-repository --module-subset=qtbase,qtdeclarative,qtquickcontrols,qtquickcontrols2 || exit /b
 cd ..
