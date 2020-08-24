fae06dcd5267ce36c6bc7c3dcf70fe1a6abf1eca
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-08-24 11:24:08 +0200

    Build Qt with SVG support

--- tools/windows/get-qt.bat
+++ tools/windows/get-qt.bat
@@ -2,6 +2,6 @@ pushd %CRYPTOSHARK_PARENTDIR%
 git clone git://code.qt.io/qt/qt5.git || exit /b
 pushd qt5
 git checkout 5.15 || exit /b
-perl init-repository --module-subset=qtbase,qtdeclarative,qtgraphicaleffects,qtquickcontrols2,qttools || exit /b
+perl init-repository --module-subset=qtbase,qtdeclarative,qtgraphicaleffects,qtquickcontrols2,qtsvg,qttools || exit /b
 popd
 popd
