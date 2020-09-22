49df14d98067093c88f87886e257652199d1356e
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-09-22 02:13:39 +0200

    Add agent method for arbitrary symbolication

--- app/agent/index.ts
+++ app/agent/index.ts
@@ -4,23 +4,26 @@ import { ModuleMonitor } from "./module-monitor";
 import { ThreadMonitor } from "./thread-monitor";
 import { ThreadTracer } from "./thread-tracer";
 
-class Agent {
+class Agent implements Service {
     exports: RpcExports = {};
+    handlers = {
+        "agent:symbolicate": this.symbolicate
+    };
 
     private services: Service[] = [];
 
     constructor() {
-        const moduleMap = new ModuleMap();
+        const moduleMonitor = new ModuleMonitor();
 
         this.services.push(
             new MemoryApi(),
-            new ModuleMonitor(moduleMap),
+            moduleMonitor,
             new ThreadMonitor(),
-            new ThreadTracer(moduleMap),
+            new ThreadTracer(moduleMonitor),
         );
 
         this.exports.dispose = this.dispose.bind(this);
-        for (const service of this.services) {
+        for (const service of [this, ...this.services]) {
             for (const [name, handler] of Object.entries(service.handlers)) {
                 this.exports[name] = handler.bind(service);
             }
@@ -39,7 +42,16 @@ class Agent {
 
     dispose() {
     }
+
+    symbolicate(addresses: string[]): SymbolicateResult[] {
+        return addresses
+            .map(ptr)
+            .map(DebugSymbol.fromAddress)
+            .map(({ name }) => name ?? null);
+    }
 }
+
+export type SymbolicateResult = string | null;
 
 const agent = new Agent();
 rpc.exports = agent.exports;
